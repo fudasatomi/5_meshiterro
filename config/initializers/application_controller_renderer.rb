72d95c6c47701430d0ae9894c4440abe61e9f438
fudasatomi <imotas.aduf@gmail.com>
2020-02-10 12:51:03 +0900

    finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '31945018533948b5943eaff7d84486b5e2ef28d07705f9b07442f7b262a89f4fc71c83b5f2cacad1a7e24950e53a504972c7025e695071eac1be20e8bc3fc28c'
